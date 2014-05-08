5a505beae3483a86b318318a039dde27754b7910
Max Nowack <max@unsou.de>
2014-05-08 20:59:41 +0200

    added FfnSaver and new solution folder for radios

---
+++ PlaylistSaver.Radio.Ffn/FfnSaver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlaylistSaver.Core;
+
+namespace PlaylistSaver.Radio.Ffn
+{
+    public class FfnSaver : IPlaylistSaver
+    {
+        public static IPlaylistSaver Create()
+        {
+            return new FfnSaver();
+        }
+
+        public StartEndSpan GetAvailableTimes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PlaylistEntry> GetEntrys(DateTime time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
