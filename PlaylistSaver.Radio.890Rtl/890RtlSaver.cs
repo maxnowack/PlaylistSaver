cafadbbacd4ba9bbf2b9685dc3f698e84f612c19
Max Nowack <max@unsou.de>
2014-05-09 10:22:20 +0200

    added 890Rtl Radio

---
+++ PlaylistSaver.Radio.890Rtl/890RtlSaver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlaylistSaver.Core;
+
+namespace PlaylistSaver.Radio._890Rtl
+{
+    public class _890RtlSaver : IPlaylistSaver
+    {
+        public TimeSpan DefaultInterval { get { return TimeSpan.FromMinutes(60); } }
+
+        public StartEndSpan GetAvailableTimes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PlaylistEntry> GetEntrys(DateTime time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
