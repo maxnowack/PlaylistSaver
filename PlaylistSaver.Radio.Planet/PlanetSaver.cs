dd1a310c7d8b6a5df8e2e6ea7dab0bb2970f7071
Max Nowack <max@unsou.de>
2014-05-09 13:37:47 +0200

    added planet radio saver

---
+++ PlaylistSaver.Radio.Planet/PlanetSaver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlaylistSaver.Core;
+
+namespace PlaylistSaver.Radio.Planet
+{
+    public class PlanetSaver : IPlaylistSaver
+    {
+        public string Name
+        {
+            get { return "Planet Radio"; }
+        }
+
+        public TimeSpan DefaultInterval
+        {
+            get { return TimeSpan.FromMinutes(10); }
+        }
+
+        public StartEndSpan GetAvailableTimes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PlaylistEntry> GetEntrys(DateTime time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
