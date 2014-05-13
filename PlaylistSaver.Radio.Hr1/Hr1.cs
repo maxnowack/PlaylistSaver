968fb44528782975fc8e97dbf172d2e1b4fc02dd
Max Nowack <max.nowack@sycor.de>
2014-05-13 17:39:59 +0200

    added hr1saver

---
+++ PlaylistSaver.Radio.Hr1/Hr1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistSaver.Radio.Hr1
+{
+    using PlaylistSaver.Core;
+    using PlaylistSaver.Radio.Helpers.Playlistbase;
+
+    public class Hr1Saver : IPlaylistSaver
+    {
+        public string Name
+        {
+            get { return "HR1"; }
+        }
+
+        public TimeSpan DefaultInterval
+        {
+            get { return TimeSpan.FromDays(1); }
+        }
+
+        public static IPlaylistSaver Create()
+        {
+            return new Hr1Saver();
+        }
+
+        public StartEndSpan GetAvailableTimes()
+        {
+            return new StartEndSpan(DateTime.Now.AddMonths(-2),DateTime.Now);
+        }
+
+        public List<PlaylistEntry> GetEntrys(DateTime time)
+        {
+            return Playlistbase.GetEntries("hr1", Name, time);
+        }
+    }
+}
