3bbfab01a02883f3bf9b6355d311353b5955fef9
Max Nowack <max.nowack@sycor.de>
2014-05-13 17:15:12 +0200

    added helpers and example for playlistbase.com

---
+++ PlaylistSaver.Radio.Helpers.Playlistbase/Playlistbase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistSaver.Radio.Helpers.Playlistbase
+{
+    using System.Net;
+
+    using CsQuery;
+
+    using PlaylistSaver.Core;
+
+    public class Playlistbase
+    {
+        private const string GetEntriesUrl =
+            "http://de.playlistbase.com/de/playlist/get-playlist-songs/station/{0}/date/{1:yyyy-MM-dd}";
+
+        public static List<PlaylistEntry> GetEntries(string stationKey, string stationName, DateTime day)
+        {
+            var web = new WebClient();
+            var html = web.DownloadString(string.Format(GetEntriesUrl, stationKey, day));
+
+            if (string.IsNullOrEmpty(html.Trim())) throw new UnknownStationException();
+            if (html.Contains("Dieser online Radiosender hat keine Playlist-Informationen verf")) return new List<PlaylistEntry>();
+
+            var doc = CQ.Create(html);
+            var items = doc["tr"].ToList();
+
+            return (from item in items
+                    select CQ.Create(item.InnerHTML)
+                    into itemQuery
+                    let artist = itemQuery["span.artist-name"][0].InnerText
+                    let title = itemQuery["span.song-name"][0].InnerText
+                    let time = DateTime.Parse(string.Format("{0:yyyy-MM-dd} {1}", day, itemQuery["td.time-cell"][0].InnerText.Trim()))
+                    select new PlaylistEntry() { Artist = artist, Title = title, Time = time, Radio = stationName })
+                .ToList();
+        }
+    }
+}
---
+++ PlaylistSaver.Radio.Helpers.Playlistbase/UnknownStationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlaylistSaver.Radio.Helpers.Playlistbase
+{
+    public class UnknownStationException : Exception
+    {
+        public UnknownStationException() : base("The provided Station is unknown!") {}
+    }
+}
