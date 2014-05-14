1f9e0ae32b04f20fde0c4ff7f46d6e0aaeafcd23
Max Nowack <max@unsou.de>
2014-05-14 07:25:49 +0200

    added saver for njoy via playlistbase

---
+++ PlaylistSaver.Radio.NjoyPb/NjoyPbSaver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistSaver.Radio.NjoyPb
+{
+    using PlaylistSaver.Core;
+    using PlaylistSaver.Radio.Helpers.Playlistbase;
+
+    public class NjoyPbSaver : IPlaylistSaver
+    {
+        public string Name
+        {
+            get { return "N-Joy"; }
+        }
+
+        public TimeSpan DefaultInterval
+        {
+            get { return TimeSpan.FromDays(1); }
+        }
+
+        public static IPlaylistSaver Create()
+        {
+            return new NjoyPbSaver();
+        }
+
+        public StartEndSpan GetAvailableTimes()
+        {
+            return new StartEndSpan(DateTime.Now.AddMonths(-2), DateTime.Now);
+        }
+
+        public List<PlaylistEntry> GetEntrys(DateTime time)
+        {
+            return Playlistbase.GetEntries("n-joy", Name, time);
+        }
+    }
+}
