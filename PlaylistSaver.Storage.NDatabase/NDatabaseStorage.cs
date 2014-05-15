0dd03c7ec2f4c7eb6ea1fbd622419a0d98f10040
Max Nowack <max@unsou.de>
2014-05-15 13:27:27 +0200

    tried to optimized ndb performance

--- PlaylistSaver.Storage.NDatabase/NDatabaseStorage.cs
+++ PlaylistSaver.Storage.NDatabase/NDatabaseStorage.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NDatabase.Api;
+using NDatabase.Exceptions;
 
 namespace PlaylistSaver.Storage.NDatabase
 {
-    using System.Security.Cryptography;
 
     using global::NDatabase;
 
@@ -14,33 +15,59 @@ namespace PlaylistSaver.Storage.NDatabase
 
     public class NDatabaseStorage : IPlaylistStorage
     {
+        private IOdb odb = null;
         public static IPlaylistStorage Create()
         {
             return new NDatabaseStorage();
         }
 
+        public NDatabaseStorage()
+        {
+            //OdbConfiguration.EnableBTreeValidation();
+            odb = OdbFactory.Open(DbFileName);
+        }
+
         private const string DbFileName = "playlist.db";
         public void Store(IEnumerable<PlaylistEntry> entries)
         {
-            using (var odb = OdbFactory.Open(DbFileName))
+            foreach (var entry in from entry in entries let results = (from e in odb.AsQueryable<PlaylistEntry>() where e.Equals(entry) select e) where !results.Any() select entry)
             {
-                foreach (var entry in from entry in entries let results = (from e in odb.AsQueryable<PlaylistEntry>() where e.Equals(entry) select e) where !results.Any() select entry)
-                {
-                    odb.Store(entry);
-                }
+                odb.Store(entry);
             }
         }
+        //public void Store(IEnumerable<PlaylistEntry> entries)
+        //{
+        //    if (!odb.IndexManagerFor<PlaylistEntry>().ExistIndex("playlistIndex"))
+        //    {
+        //        odb.IndexManagerFor<PlaylistEntry>()
+        //            .AddUniqueIndexOn("playlistIndex", new[] {"Radio", "Time", "Artist", "Title"});
+        //    }
 
+        //    foreach (var entry in entries)
+        //    {
+        //        try
+        //        {
+        //            odb.Store(entry);
+        //        }
+        //        catch (DuplicatedKeyException duplicatedKeyException)
+        //        {
+        //            odb.Dispose();
+        //            odb = OdbFactory.Open(DbFileName);
+        //        }
+        //    }
+        //}
+
+        ~NDatabaseStorage()
+        {
+            odb.Dispose();
+        }
 
         public DateTime GetLastEntry(string stationKey)
         {
-            using (var odb = OdbFactory.Open(DbFileName))
-            {
-                return (from e in odb.QueryAndExecute<PlaylistEntry>()
-                        where e.Radio == stationKey
-                        orderby e.Time descending
-                        select e.Time).FirstOrDefault();
-            }
+            return (from e in odb.QueryAndExecute<PlaylistEntry>()
+                    where e.Radio == stationKey
+                    orderby e.Time descending
+                    select e.Time).FirstOrDefault();
         }
     }
 }
