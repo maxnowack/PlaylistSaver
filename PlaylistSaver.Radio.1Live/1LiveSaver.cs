d1e8c1ae7f64089b1f428941c01169a169917a1c
Max Nowack <max@unsou.de>
2014-06-09 21:44:03 +0200

    optimized 1live saver

--- PlaylistSaver.Radio.1Live/1LiveSaver.cs
+++ PlaylistSaver.Radio.1Live/1LiveSaver.cs
@@ -7,43 +7,32 @@ using System.Threading.Tasks;
 namespace PlaylistSaver.Radio._1Live
 {
     using PlaylistSaver.Core;
+    using PlaylistSaver.Radio.Helpers.Basic;
     using PlaylistSaver.Radio.Helpers.Playlistbase;
+    using System.Globalization;
 
-    public class _1LiveSaver : IPlaylistSaver
+    public class _1LiveSaver : BasicSaver
     {
-        private const string StationName = "1Live";
-
-        private string StationKey
-        {
-            get
-            {
-                return StationName.ToLower();
-            }
-        }
-
-        public string Name
-        {
-            get { return StationName; }
-        }
-
-        public TimeSpan DefaultInterval
-        {
-            get { return TimeSpan.FromDays(1); }
-        }
-
+        public _1LiveSaver() : base("1Live", TimeSpan.FromMinutes(10)) { }
         public static IPlaylistSaver Create()
         {
             return new _1LiveSaver();
         }
-
-        public StartEndSpan GetAvailableTimes()
+        public override StartEndSpan GetAvailableTimes()
         {
-            return new StartEndSpan(DateTime.Now.AddMonths(-1), DateTime.Now);
+            return new StartEndSpan(DateTime.Now.AddYears(-1), DateTime.Now);
         }
-
-        public List<PlaylistEntry> GetEntrys(DateTime time)
+        public override List<PlaylistEntry> GetEntrys(DateTime time)
         {
-            return Playlistbase.GetEntries(StationKey, Name, time);
+            var days = (DateTime.Now - time).Days;
+            var html = GetWebContent(string.Format("http://www.einslive.de/musik/playlists/index.jsp?wday={1}&hour={0:HH}&minute={0:mm}", time, days));
+            
+            return GetEntrys(time,
+                html,
+                doc => doc["table[summary^='Die am'] tbody tr"].ToList(),
+                cq => DateTime.ParseExact(cq["td"][0].InnerText, "ddd MMM dd HH:mm:ss CEST yyyy", CultureInfo.InvariantCulture),
+                cq => cq["td"][1].InnerText,
+                cq => cq["td"][2].InnerText);
         }
     }
 }
